89770135f92ce78bf5b8f2dcaa3d3a8c4d9e550d
Min He <minhe.86@gmail.com>
2025-02-21 15:09:28 +1100

    test: attach screenshot and test data into allure

--- cucumber.mjs
+++ cucumber.mjs
@@ -7,15 +7,13 @@ export default {
     "step-definitions/*.step.js",
   ],
   requireModule: ["allure-cucumberjs"],
-  formatOptions: {
-    snippetInterface: "async-await",
-  },
-  dryRun: false,
   // allure reporter config for cucumberJS pls refer to https://github.com/allure-framework/allure-js/tree/main/packages/allure-cucumberjs
   format: ["allure-cucumberjs/reporter"],
   formatOptions: {
+    snippetInterface: "async-await",
     resultsDir: "allure-results",
   },
+  dryRun: false,
   parallel: 2,
   tags: process.env.TEST_TAGS || "@ui or @api or @gql",
 };
